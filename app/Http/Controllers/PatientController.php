ad6be9853a31f23fa640e5440eb00c3ed288a988
Moin Sarwar <moinsarwar19@gmail.com>
2026-06-15 07:22:45 +0500

    refund add

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -143,7 +143,7 @@ class PatientController extends Controller
 
                 $session = $safepay->order->setup([
                     "merchant_api_key" => $apiKey,
-                    "intent" => "CYBERSOURCE",
+                    "intent" => "SAFEPAY",
                     "mode" => "payment",
                     "currency" => "PKR",
                     "amount" => (int) ($appointment->fee * 100), // Minor units
@@ -203,7 +203,7 @@ class PatientController extends Controller
 
                 $session = $safepay->order->setup([
                     "merchant_api_key" => $apiKey,
-                    "intent" => "CYBERSOURCE",
+                    "intent" => "SAFEPAY",
                     "mode" => "payment",
                     "currency" => "PKR",
                     "amount" => (int) ($appointment->fee * 100),
